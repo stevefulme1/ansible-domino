cce9b63e060d15268498325b90262b7214e008f2
sfulmer <sfulmer@redhat.com>
2026-05-18 18:25:19 -0400

    fix: remove no_log from docs, ensure __init__.py

--- plugins/doc_fragments/common.py
+++ plugins/doc_fragments/common.py
@@ -25,7 +25,6 @@ options:
             - Can also be set via the C(DOMINO_API_KEY) environment variable.
         type: str
         required: true
-        no_log: true
     validate_certs:
         description:
             - Whether to validate SSL certificates.
